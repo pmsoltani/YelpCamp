d5b4a5ec8341137607fe8ab384951564a7edc6ab
Pooria Soltani <pooria.ms@gmail.com>
2018-11-11 23:20:29 +0330

    added a form to post new campgrounds

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
 // Vars
@@ -8,7 +10,7 @@ var campgrounds = [
     {name: 'Salmon Creek', iamge: 'https://www.reserveamerica.com/webphotos/NH/pid270015/0/540x360.jpg'},
     {name: 'Granite Hill', iamge: 'https://www.nps.gov/shen/planyourvisit/images/20170712_A7A9022_nl_Campsites_BMCG_960.jpg?maxwidth=1200&maxheight=1200&autorotate=false'},
     {name: 'Mountain Retreat', image: 'https://www.campjellystone.com/wp/wp-content/uploads/2012/08/PennsylvaniaCampgrounds1.jpg'}
-]
+];
 
 // Routes
 app.get('/', (req, res) => {
@@ -17,6 +19,19 @@ app.get('/', (req, res) => {
 
 app.get('/campgrounds', (req, res) => {
     res.render('campgrounds', {campgrounds: campgrounds});
+});
+
+app.post('/campgrounds', (req, res) => {
+    // get data from form and add to campgrounds array
+    const name = req.body.name;
+    const image = req.body.image;
+    campgrounds.push({name: name, image: image});
+    // redirect back to campgrounds page
+    res.redirect('/campgrounds');
+});
+
+app.get('/campgrounds/new', (req, res) => {
+    res.render('new');
 });
 
 // Server
